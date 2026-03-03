9984f1dcef95b43fc41cf260f4dd620af05910c5
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-03 17:05:58 +0100

    Fix: Sync models and init for Zadarma call

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,13 +1,9 @@
 from odoo import models, fields, api
-import logging
-
-_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     def action_zadarma_call(self):
-        """Метод для кнопки виклику Zadarma"""
+        """Метод, який викликається кнопкою з інтерфейсу"""
         self.ensure_one()
-        _logger.info(">>> Ініціація дзвінка для: %s", self.name)
-        return True
+        return self.env['zadarma.api'].make_callback(self.phone or self.mobile)
