69f8985a1925dce8cecbe7febd3cdf2737e2a7d0
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-03 17:09:57 +0100

    Fix: add missing zadarma.call model

--- models/zadarma_call.py
+++ models/zadarma_call.py
@@ -1,9 +1,22 @@
-from odoo import models, api
+from odoo import models, fields, api
+
+class ZadarmaCall(models.Model):
+    _name = 'zadarma.call'
+    _description = 'Zadarma Call Log'
+
+    name = fields.Char(string='Call ID')
+    caller_number = fields.Char(string='Caller Number')
+    called_number = fields.Char(string='Called Number')
+    duration = fields.Integer(string='Duration (sec)')
+    state = fields.Selection([
+        ('success', 'Success'),
+        ('failed', 'Failed')
+    ], string='Status', default='success')
 
 class ZadarmaAPI(models.AbstractModel):
     _name = 'zadarma.api'
     _description = 'Zadarma API Helper'
 
     def make_callback(self, partner_phone):
-        """Метод для ініціації дзвінка (поки що пуста логіка)"""
+        """Метод для ініціації дзвінка"""
         return {'status': 'success'}
