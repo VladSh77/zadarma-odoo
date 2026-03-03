0691882a6768d119f875f7a68ed2acd5569c9d43
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-03 17:18:04 +0100

    Fix: Add all missing fields for zadarma.call model

--- models/zadarma_call.py
+++ models/zadarma_call.py
@@ -8,6 +8,10 @@ class ZadarmaCall(models.Model):
     call_id = fields.Char(string='Zadarma Call ID')
     caller_number = fields.Char(string='Caller Number')
     called_number = fields.Char(string='Called Number')
+    call_type = fields.Selection([
+        ('inbound', 'Inbound'),
+        ('outbound', 'Outbound')
+    ], string='Call Type', default='outbound')
     start_time = fields.Datetime(string='Start Time', default=fields.Datetime.now)
     end_time = fields.Datetime(string='End Time')
     duration = fields.Integer(string='Duration (sec)')
@@ -15,6 +19,8 @@ class ZadarmaCall(models.Model):
         ('success', 'Success'),
         ('failed', 'Failed')
     ], string='Status', default='success')
+    partner_id = fields.Many2one('res.partner', string='Partner')
+    user_id = fields.Many2one('res.users', string='Responsible', default=lambda self: self.env.user)
 
 class ZadarmaAPI(models.AbstractModel):
     _name = 'zadarma.api'
