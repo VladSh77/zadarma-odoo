7eb693179331e053c6dcd9bb31453d0eb172c52c
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-04 15:10:24 +0100

    Feature: Added SIP ID to user settings for Click-to-Call

--- models/res_users.py
+++ models/res_users.py
@@ -3,7 +3,4 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    zadarma_internal_number = fields.Char(
-        string='Zadarma SIP', 
-        help="Внутрішній номер АТС (наприклад, 101)"
-    )
+    zadarma_sip = fields.Char(string='Zadarma SIP ID', help="Ваш SIP номер (наприклад, 123456)")
