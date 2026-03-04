00e306028f4961c8427408a4272d96e5ce659805
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-04 15:21:02 +0100

    Fix: Added missing zadarma fields to res.company model

--- models/res_company.py
+++ models/res_company.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    zadarma_api_key = fields.Char(string='Zadarma API Key')
-    zadarma_api_secret = fields.Char(string='Zadarma API Secret')
+    zadarma_key = fields.Char(string='Zadarma Key')
+    zadarma_secret = fields.Char(string='Zadarma Secret')
