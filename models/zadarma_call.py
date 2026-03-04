9547ed0c0587c48c4bdc1a9e331a87faf810e1b9
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-04 15:06:52 +0100

    UI: Added computed name to calls and improved list visibility

--- models/zadarma_call.py
+++ models/zadarma_call.py
@@ -1,10 +1,11 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ZadarmaCall(models.Model):
     _name = 'zadarma.call'
     _description = 'Zadarma Call'
     _order = 'date_start desc'
 
+    name = fields.Char(string='Заголовок', compute='_compute_name', store=True)
     call_id = fields.Char(string='Call ID', index=True)
     date_start = fields.Datetime(string='Дата початку')
     phone_number = fields.Char(string='Номер телефону', index=True)
@@ -17,3 +18,9 @@ class ZadarmaCall(models.Model):
     partner_id = fields.Many2one('res.partner', string='Контакт')
     lead_id = fields.Many2one('crm.lead', string='Лід')
     user_id = fields.Many2one('res.users', string='Відповідальний')
+
+    @api.depends('phone_number', 'date_start')
+    def _compute_name(self):
+        for record in self:
+            date_str = record.date_start.strftime('%Y-%m-%d %H:%M') if record.date_start else 'Н/Д'
+            record.name = f"Дзвінок {record.phone_number} ({date_str})"
