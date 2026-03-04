002fb762da9a230bc4e84a5e81bd68380030509b
Volodymyr Shevchenko <vladsh.ua@icloud.com>
2026-03-04 14:26:03 +0100

    Fix: Removed obsolete zadarma_sip_mapping field from company model and view

--- models/res_company.py
+++ models/res_company.py
@@ -4,11 +4,4 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     zadarma_api_key = fields.Char(string='Zadarma API Key')
-    zadarma_api_secret = fields.Char(string='Zadarma API Secret', password=True)
-    zadarma_sandbox = fields.Boolean(string='Sandbox Mode', default=False)
-    
-    # SIP Mapping: відповідність внутрішніх номерів (101) ID користувачів Odoo
-    zadarma_sip_mapping = fields.Text(
-        string='SIP Mapping', 
-        help="Format: SIP:User_ID (e.g., 101:2, 102:5). Used to assign leads to users."
-    )
+    zadarma_api_secret = fields.Char(string='Zadarma API Secret')
